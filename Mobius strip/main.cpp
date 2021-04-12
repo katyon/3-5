7e3586a4a8e993f0b35f3e6ba61b26b2b0e66720
ombria <bakugou2011@gmail.com>
2021-04-12 19:44:50 +0900

    menu

--- Mobius strip/main.cpp
+++ Mobius strip/main.cpp
@@ -15,18 +15,76 @@ void TyDamoPlay();
 #include "StageManager.h"
 #include "Balance.h"
 #include "ScreenRecord.h"
+#include "menu.h"
+Menu menu;
 /********************************************************************/
 
 INT WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, INT)
 {
-	
-	AliceLib::Entry(L"AliceLib", 1280, 720, DefaultWindowMode::WM_WINDOW, 60);	
+    //ライブラリの初期化処理
+    AliceLib::Entry(L"AliceLib", 1920, 1080, DefaultWindowMode::WM_FULLSCREEN, 60);
 
-	while (Function::GameLoop())
-	{
-		
-		AliceLib::Present(0u, 0u);
-	}
+    //複数箇所で使うため用意しておいたほうがいい
+    int scene = AliceLib::GetNowScene();
 
-	return AliceLib::Exit();
+    //ゲームループ
+    while (Function::GameLoop())
+    {
+        //現在のシーンを取得する
+        scene = AliceLib::GetNowScene();
+
+        //前のシーンと現在のシーンが違うとき
+        if (AliceLib::InitFlg())
+        {
+            //初期化処理
+            switch (scene)
+            {
+            default:
+                break;
+            case 0:
+                menu.init();
+                break;
+            }
+        }
+
+        //更新処理
+        switch (scene)
+        {
+        default:
+            break;
+        case 0:
+           menu.update();
+            break;
+        }
+
+        //描画処理
+        switch (scene)
+        {
+        default:
+            break;
+        case 0:
+            menu.draw();
+            break;
+        }
+
+        //シーンが変更されるとき
+        if (AliceLib::EndFlg())
+        {
+            //終了処理
+            switch (scene)
+            {
+            default:
+                break;
+            case 0:
+                break;
+            }
+        }
+
+        //バックバッファに送信
+        AliceLib::Present(0u, 0u);
+
+    }
+
+    //ライブラリの終了処理
+    return AliceLib::Exit();
 }
