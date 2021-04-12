61c1f09ee54a9b1f0f9a5eb666fc95338790251e
izanagi7755 <izanagi7755@gmail.com>
2021-04-12 22:26:46 +0900

    水道管パズルシステム .cpp、.hの追加 メインにコメントアウトあり ※.pngを除いているので、別途入れないと実行不可

--- Mobius strip/main.cpp
+++ Mobius strip/main.cpp
@@ -15,16 +15,19 @@ void TyDamoPlay();
 #include "StageManager.h"
 #include "Balance.h"
 #include "ScreenRecord.h"
+#include "PipePuzzle.h"
 /********************************************************************/
+//PipePuzzle pp; 
 
 INT WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, INT)
 {
 	
 	AliceLib::Entry(L"AliceLib", 1280, 720, DefaultWindowMode::WM_WINDOW, 60);	
 
+	//pp.Init();
 	while (Function::GameLoop())
 	{
-		
+		//pp.run();
 		AliceLib::Present(0u, 0u);
 	}
 
