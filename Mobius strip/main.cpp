550f1abf4ee8df4cf8806101d64bb899c23c61f8
katyo <kattyon0321@gmail.com>
2021-04-15 23:14:58 +0900

    main

--- Mobius strip/main.cpp
+++ Mobius strip/main.cpp
@@ -20,8 +20,8 @@ void TyDamoPlay();
 #include "PipePuzzle.h"
 #include "menu.h"
 Menu menu;
-/********************************************************************/
 //PipePuzzle pp; 
+/********************************************************************/
 
 INT WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, INT)
 {
@@ -83,80 +83,14 @@ INT WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, INT)
 
 		//バックバッファに送信
 	//pp.Init();
-	while (Function::GameLoop())
-	{
-		//pp.run();
-		AliceLib::Present(0u, 0u);
+		while (Function::GameLoop())
+		{
+			//pp.run();
+			AliceLib::Present(0u, 0u);
 
+		}
+
+		//ライブラリの終了処理
+		return AliceLib::Exit();
 	}
-
-	//ライブラリの終了処理
-	return AliceLib::Exit();
-}
-    //ライブラリの初期化処理
-    AliceLib::Entry(L"AliceLib", 1920, 1080, DefaultWindowMode::WM_FULLSCREEN, 60);
-
-    //複数箇所で使うため用意しておいたほうがいい
-    int scene = AliceLib::GetNowScene();
-
-    //ゲームループ
-    while (Function::GameLoop())
-    {
-        //現在のシーンを取得する
-        scene = AliceLib::GetNowScene();
-
-        //前のシーンと現在のシーンが違うとき
-        if (AliceLib::InitFlg())
-        {
-            //初期化処理
-            switch (scene)
-            {
-            default:
-                break;
-            case 0:
-                menu.init();
-                break;
-            }
-        }
-
-        //更新処理
-        switch (scene)
-        {
-        default:
-            break;
-        case 0:
-           menu.update();
-            break;
-        }
-
-        //描画処理
-        switch (scene)
-        {
-        default:
-            break;
-        case 0:
-            menu.draw();
-            break;
-        }
-
-        //シーンが変更されるとき
-        if (AliceLib::EndFlg())
-        {
-            //終了処理
-            switch (scene)
-            {
-            default:
-                break;
-            case 0:
-                break;
-            }
-        }
-
-        //バックバッファに送信
-        AliceLib::Present(0u, 0u);
-
-    }
-
-    //ライブラリの終了処理
-    return AliceLib::Exit();
 }
