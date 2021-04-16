3809d1827f6cb1c38f40eccde8da9382a192ea48
ReliefAmbient <shadowbutterfly1925@gmail.com>
2021-04-16 10:46:46 +0900

    push

--- Mobius strip/fps_camera.h
+++ Mobius strip/fps_camera.h
@@ -10,24 +10,26 @@ private:
 		FLOAT2 current_pos;
 	}Cursor;
 
+	FLOAT3 focus_point;
+
 	VECTOR3D forward;
 	VECTOR3D relative_pos;
 
 	XMMATRIX rotate_to_target;
 	int magnification = 15;
-	static int correct_sensitivity;
-	bool useADS;
+	//static int correct_sensitivity;
+
 private:
-	//VECTOR2D getCursorMoveAmount();
+	VECTOR2D getCursorMoveAmount();
 public:
 	// TODO:プレイヤーの視点方向にカメラを向ける処理
 
 	FPSCamera();
-	void initPos();
+	void init();
 	void update(XMMATRIX player_world_matrix, FLOAT3 center);
 
 	VECTOR3D getForward() { return forward; }
 	void zoom();
-	bool getUseADS() { return useADS; }
-	void setSensitivity(int s) { correct_sensitivity = s; }
+	//bool getUseADS() { return useADS; }
+	//void setSensitivity(int s) { correct_sensitivity = s; }
 };
--- Mobius strip/main.cpp
+++ Mobius strip/main.cpp
@@ -22,6 +22,8 @@ INT WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, INT)
 {
 	
 	AliceLib::Entry(L"AliceLib", 1280, 720, DefaultWindowMode::WM_WINDOW, 60);
+
+	//SpriteLoad(1, L"Data/Sprite/center.png");
 	//Camera camera;
 	//camera.SetPos({ 0, 0, -30 });
 
@@ -30,10 +32,15 @@ INT WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, INT)
 
 	while (Function::GameLoop())
 	{
-		//player.update();
-		//camera.SetTarget({ 0.0f,5.0f,0.0f });
+		/*player.update();
+		camera.SetTarget({ 0.0f,5.0f,0.0f });
 
-		//player.render(camera);
+		player.render(camera);
+
+		FLOAT2 center = ToClient(GetWindowSize() / 2.0f);
+		SetCursorPos(center.x, center.y);
+
+		SpriteRender(1, (GetWindowSize() / 2.0f), { 0.2f, 0.2f }, { 0, 0 }, { 0, 0 }, { 300.0f, 400.0f });*/
 		AliceLib::Present(0u, 0u);
 	}
 
