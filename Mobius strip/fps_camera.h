6b2ca5b4782fb6642c9f89f8b7a309ca9a0f23f7
ReliefAmbient <shadowbutterfly1925@gmail.com>
2021-04-16 15:18:54 +0900

    ぷしゅー

--- Mobius strip/fps_camera.h
+++ Mobius strip/fps_camera.h
@@ -11,9 +11,9 @@ private:
 	}Cursor;
 
 	FLOAT3 focus_point;
+	VECTOR3D relative_focus_point;
 
 	VECTOR3D forward;
-	VECTOR3D relative_pos;
 
 	XMMATRIX rotate_to_target;
 	int magnification = 15;
@@ -29,7 +29,28 @@ public:
 	void update(XMMATRIX player_world_matrix, FLOAT3 center);
 
 	VECTOR3D getForward() { return forward; }
-	void zoom();
+	//void zoom();
 	//bool getUseADS() { return useADS; }
 	//void setSensitivity(int s) { correct_sensitivity = s; }
 };
+
+// ---kiira_develop---
+// 
+//SpriteLoad(1, L"Data/Sprite/center.png");
+//Camera camera;
+//camera.SetPos({ 0, 0, -30 });
+
+//Player player;
+//player.init();
+
+
+//player.update();
+//camera.SetTarget({ 0.0f,5.0f,0.0f });
+//
+//player.render(camera);
+//
+//FLOAT2 center = ToClient(GetWindowSize() / 2.0f);
+//SetCursorPos(center.x, center.y);
+//
+//SpriteRender(1, (GetWindowSize() / 2.0f), { 0.2f, 0.2f }, { 0, 0 }, { 0, 0 }, { 300.0f, 400.0f });
+//--------------------------
