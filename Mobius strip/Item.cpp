95c491e895afe1f2f9199aed2904afe1110a6439
ombria <bakugou2011@gmail.com>
2021-04-22 02:14:55 +0900

    アイテム取得(メモだけ)

--- Mobius strip/Item.cpp
+++ Mobius strip/Item.cpp
@@ -1,54 +1,94 @@
 #include "Item.h"
+#include "Shelter.h"
 
-void GameItem::init()
+void ItemObj::init()
 {
-
-
+    kami.load("Data\\Objects\\kami.fbx");
+    pos = { 0,4,0 };
+    scale = { 1,1,1 };
+    OBBscale = { 10.0f,10.0f,10.0f };
 }
 
-void GameItem::isChoice()
+void ItemObj::update(const Camera& camera)
 {
-    if (!input::GetWheel())return;
+    getMouseRay(camera, rayStart, rayEnd);
 
-    select = true;
-
-    for (auto i : items)
+    if (ColLineOBB(rayStart, rayEnd, CreateOBB(pos, OBBscale, posture), hitPos))
     {
-        if (ColRects(pos[i].y, pos[i].y + 120.0f, pos[i].x, pos[i].x + 120.0f, input::GetMousePos()))
+        if (input::TRG(VK_LBUTTON))
         {
-            //アイテムを使用
-            //アイテムの種類ごとの処理を書くこと
-            switch (use_item(i))
-            {
-            case ID_ITEM1:
-                break;
-            case ID_ITEM2:
-                break;
-            case ID_ITEM3:
-                break;
-            case ID_ITEM4:
-                break;
-            case ID_EMPTY:
-                break;
-            }
+            get_item(ITEM_ID::ID_ITEM1);
         }
     }
 }
 
-
-
-
-// マウスホイールでアイテムを使用する版
-void GameItem::update()
+void ItemObj::render(const Camera& camera)
 {
-    GameItem::isChoice();
-
-
-
+    COLOR color = { 1.0f,1.0f,1.0f,1.0f };
+    posture.reset();
+    if (exist[0])
+    {
+        SkinnedMeshRender(kami, camera, pos, scale, posture, camera.LightFloamCamera(), color);
+    }
 }
 
-
-void GameItem::draw()
-{
-
-}
+//GameItem::GameItem()
+//{
+//    kami.load("Data\\Objects\\kami.fbx");
+//}
+//
+//void GameItem::init()
+//{
+//    scale = ScalarToFloat3(0.165f);
+//    OBBscale = ScalarToFloat3(2.5f);
+//}
+//
+//void GameItem::isChoice()
+//{
+//    if (!input::GetWheel())return;
+//
+//    select = true;
+//
+//    for (auto i : items)
+//    {
+//        if (ColRects(pos[i].y, pos[i].y + 120.0f, pos[i].x, pos[i].x + 120.0f, input::GetMousePos()))
+//        {
+//            //アイテムを使用
+//            //アイテムの種類ごとの処理を書くこと
+//            switch (use_item(i))
+//            {
+//            case ID_ITEM1:
+//                break;
+//            case ID_ITEM2:
+//                break;
+//            case ID_ITEM3:
+//                break;
+//            case ID_ITEM4:
+//                break;
+//            case ID_EMPTY:
+//                break;
+//            }
+//        }
+//    }
+//}
+//
+//
+//
+//
+//// マウスホイールでアイテムを使用する版
+//void GameItem::update()
+//{
+//    GameItem::isChoice();
+//
+//
+//
+//}
+//
+//
+//void GameItem::draw()
+//{
+//    if (select)
+//    {
+//        SpriteRender(item_ptr, 100, 100, 1, 1, 0, 0, 120, 120, 0, 0, 0, 1, 1, 1, 1);
+//    }
+//}
--- Mobius strip/SceneGame.cpp
+++ Mobius strip/SceneGame.cpp
@@ -11,7 +11,7 @@ SceneGame::SceneGame() /*: pipe_puzzle()*/
     player.init();
     stage.load("Data/Objects/stage.fbx");
     SpriteLoad(1, L"Data/Sprite/center.png");
-
+    itemObj->init();
     // ボタンプッシュ ここから
     //camera.SetPos({ 0,200,-10 });
     //camera.SetTarget({ 0,0,0 });
@@ -37,60 +37,59 @@ void SceneGame::Initialize()
     camera.initPos();
     camera.SetTarget({ 0, 0, 5 });
     player.init();
+    itemObj->init();
+
 }
 
 //シーン全体の更新処理
 //経過時間が渡されます
 void SceneGame::Update(float elapsed_time)
 {
- 
-
-        switch (game_mode)
+    switch (game_mode)
+    {
+    case normal:
+        // ボタンプッシュ
+        //ButtonPush::getInstance()->update(camera);
+        //pipe_puzzle.Update();
+        if (input::TRG('P'))
         {
-        case normal:
-            // ボタンプッシュ
-            //ButtonPush::getInstance()->update(camera);
-            //pipe_puzzle.Update();
-            if (input::TRG('P'))
+            for (int i = 0; i < screenR->max_racord; i++)
             {
-                for (int i = 0; i < screenR->max_racord; i++)
+                if (!screenR->records[i])
                 {
-                    if (!screenR->records[i])
-                    {
-                        screenR->save(i);
-                        break;
-                    }
+                    screenR->save(i);
+                    break;
                 }
             }
-
-            camera.update(GetWorldMatrix((player.getPos() + FLOAT3(0, 12.5f, 0)), FLOAT3(1, 1, 1), { 0,0,0 }), { player.getPos().x, player.getPos().y + 12.5f, player.getPos().z });
-
-            if (input::TRG(VK_TAB))
-            {
-                menu.isPause = true;
-                menu.tab = MenuTab::Item;
-                game_mode = menue;
-            }
-            break;
-
-        case menue:
-            if (input::TRG(VK_TAB))
-            {
-                game_mode = normal;
-            }
-            if (menu.isPause)
-            {
-                menu.update();
-                M_Item->update();
-            }
-            G_Item->update();
-            break;
-        case balance:
-
-            break;
         }
 
+        camera.update(GetWorldMatrix((player.getPos() + FLOAT3(0, 12.5f, 0)), FLOAT3(1, 1, 1), { 0,0,0 }), { player.getPos().x, player.getPos().y + 12.5f, player.getPos().z });
+        if (input::TRG(VK_TAB))
+        {
+            menu.isPause = true;
+            menu.tab = MenuTab::Item;
+            game_mode = menue;
+        }
+        break;
+
+    case menue:
+        if (input::TRG(VK_TAB))
+        {
+            game_mode = normal;
+        }
+        if (menu.isPause)
+        {
+            menu.update();
+            M_Item->update();
+        }
+        break;
+    case balance:
+
+        break;
+    }
+
     player.update(camera);
+    itemObj->update(camera);
     Debug->SetString("カメラ回転中心座標 %f %f %f", player.getPos().x, player.getPos().y + 12.5f, player.getPos().z);
     Debug->SetString("カメラ座標 %f %f %f", camera.GetPos().x, camera.GetPos().y, camera.GetPos().z);
     Debug->SetString("カメラの距離 %f", FLOAT3::distanceFrom({ player.getPos().x, player.getPos().y + 12.5f, player.getPos().z }, camera.GetPos()));
@@ -103,10 +102,11 @@ void SceneGame::Render()
     {
     case normal:
         screenR->begin();
-         player.render(camera);
-         SkinnedMeshRender(stage, camera, GetWorldMatrix({ 0,0,0 }, { 0.1,0.1,0.1 }, { 0,0,0 }), camera.LightFloamCamera());
-         SpriteRender(1, (GetWindowSize() / 2.0f), { 0.2f, 0.2f }, { 0, 0 }, { 0, 0 }, { 300.0f, 400.0f });
+        player.render(camera);
+        SkinnedMeshRender(stage, camera, GetWorldMatrix({ 0,0,0 }, { 0.1,0.1,0.1 }, { 0,0,0 }), camera.LightFloamCamera()); 
+        SpriteRender(1, (GetWindowSize() / 2.0f), { 0.2f, 0.2f }, { 0, 0 }, { 0, 0 }, { 300.0f, 400.0f });
          screenR->end();
+
         break;
 
     case menue:
@@ -116,7 +116,6 @@ void SceneGame::Render()
             menu.draw();
         }
         screenR->end();
-        G_Item->draw();
         break;
     case balance:
 
@@ -124,7 +123,9 @@ void SceneGame::Render()
     }
     // ボタンプッシュ
     //ButtonPush::getInstance()->Render(camera);
-    //pipe_puzzle.Render(camera);
+    itemObj->render(camera);
+
+    pipe_puzzle.Render(camera);
 
 
 }
